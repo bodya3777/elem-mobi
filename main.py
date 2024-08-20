a9e03542dbf8b3f3d1b8a7520c43a93cf3a3b57e
bogdan <user@MacBook-Air-User.local>
2024-08-20 22:37:13 +0300

    fix main.py

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ with open('database/accounts.txt', 'r') as file:
         password = line.replace(login+':', '')
         bots.append(Bot(login, password))
 
+def bots_donate():
+    for bot in bots:
+        bot.donate()
 
+def bots_do_duels():
+    for bot in bots:
+        bot.do_duels()
 
 if __name__ == '__main__':
     schedule.every().day.do(bots_donate)
-    schedule.every(600).minutes.do(bots_donate)
+    schedule.every(600).minutes.do(bots_do_duels)
     while True:
         schedule.run_pending()
         time.sleep(1)
